5c2ee1448581b84d8be97e1067fb6d8c3144117b
Albert Mateos <mateos.aguilera.albert@alumnat.copernic.cat>
2023-03-28 17:04:28 +0200

    commit bootstrap

--- src/main/java/com/ourERP/knightvision/controller/HomeController.java
+++ src/main/java/com/ourERP/knightvision/controller/HomeController.java
@@ -16,12 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     
     @GetMapping("/registrousers")
-    public String irAOtraPagina() {
+    public String irARegistroUsers() {
         return "registrousers"; 
     }
     
     @GetMapping("/eventos")
-    public String eventos(){
+    public String irAEventos(){
         return "eventos";
     }
 }
