2a339eb50a4f477460d482237fab4273b5d59d6d
Albert Mateos <mateos.aguilera.albert@alumnat.copernic.cat>
2023-04-11 18:17:31 +0200

    commit popup

---
+++ src/main/java/clases/usuario/Eventos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases.usuario;
+
+/**
+ *
+ * @author alber
+ */
+
+public class Eventos {
+    private int id;
+    private String nombre;
+    
+    
+    
+}
